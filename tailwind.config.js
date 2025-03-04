a10db3502c1947e8303d7648d776a01d8fc70aca
mohitboharaimg <146337834+mohitboharaimg@users.noreply.github.com>
2025-03-04 18:08:54 +0530

    done

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        // Define custom colors
+        theme1: '#CCA106', // You can use any color name you want,
+      },
+    },
+  },
+  plugins: [],
+}
