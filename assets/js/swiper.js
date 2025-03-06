eaf94f10396476fd55958783f061cd255e1ccdbd
mohitboharaimg <146337834+mohitboharaimg@users.noreply.github.com>
2025-03-06 14:59:44 +0530

    blogs

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -52,3 +52,33 @@ function logoSwiper() {
 }
 logoSwiper();
 
+function blogSwiper() {
+    new Swiper(".blog-swiper", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        // autoplay: {
+        //     delay: 3500,
+        //     disableOnInteraction: false,
+        // },
+        breakpoints: {
+            400: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 40,
+            },
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+            },
+        },
+    });
+}
+blogSwiper();
